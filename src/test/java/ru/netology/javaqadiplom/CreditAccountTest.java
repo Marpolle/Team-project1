a68e47addc7cf91c44f2f38d58638a1f025af856
Maria Polezhaeva <mari.polezhaeva.97@mail.ru>
2024-06-14 16:35:11 +0300

    Credit change

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,79 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+    @Test
+    public void shouldRateMoreThanMax() {
+        CreditAccount account = new CreditAccount(
+                1_000,
+                4_000,
+                15_000
+        );
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+
+    @Test
+    public void shouldAddToNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                -1_000,
+                5_000,
+                15
+        );
+        account.add(0);
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToNegativeCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                1_000,
+                -5_000,
+                15
+        );
+        account.add(0);
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldInitialBalanceMoreThanCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                3_000,
+                15
+        );
+        account.add(0);
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldAmountMoreThanCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                4_000,
+                15
+        );
+        account.add(5_000);
+        Assertions.assertEquals(0, account.getBalance());
+    }
+    @Test
+    public void shouldReplenishAmount() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                8_000,
+                10
+        );
+        account.add(3_000);
+        Assertions.assertEquals(5_000, account.getBalance());
+    }
+
+    @Test
+    public void CorrectYearChange() {
+        CreditAccount account = new CreditAccount(
+                3_000,
+                5_000,
+                10
+
+        );
+        Assertions.assertEquals(300, account.yearChange());
+    }
 }
